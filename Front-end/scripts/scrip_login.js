4a51b5c25922c6ca7accf7ea11e4cf84d4c11e1b
luisdue3 <luisdue3@gmail.com>
2022-09-30 09:55:34 -0500

    scrip_login.js consultar cliente a iniciar sesion

--- Front-end/scripts/scrip_login.js
+++ Front-end/scripts/scrip_login.js
@@ -2,13 +2,23 @@ const email = document.getElementById('correo_1')
 const password = document.getElementById('contrasenia_1')
 const button = document.getElementById('boton')
 
+async function obtener_cliente (uri_cliente,correo) {
+    // Enviar petición de tipo Get
+    const cliente_resp = await fetch(uri_cliente+correo)
+    //console.log(resp)
+    const cliente_json = await cliente_resp.json()
+    //console.log(cliente_json)
+    return cliente_json
+  }
+
 button.addEventListener('click', (e) => {
     e.preventDefault();
     const data = {
         email: email.value,
         password: password.value
     }
-
-    console.log(data)
+    let uri_cliente="https://ferreteria-ecommerce-backend.herokuapp.com/api/v1/cliente/obtenerClientePorCorreo/"
+    const cliente= obtener_cliente(uri_cliente,data.email)
+    console.log(cliente)
 })
 
